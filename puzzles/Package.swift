90120f8fa39dc15fbe6e0c826db89e4a0215aa53
alejandro quesada <quesadaaba@gmail.com>
2022-12-06 00:15:16 -0800

    Simplified Day 6 implementation using Swift Algorithms - though its slower than the original LRU implementation!

--- puzzles/Package.swift
+++ puzzles/Package.swift
@@ -6,13 +6,15 @@ import PackageDescription
 let package = Package(
   name: "Puzzle",
   dependencies: [
-    .package(url: "https://github.com/apple/swift-argument-parser", from: "1.2.0")
+    .package(url: "https://github.com/apple/swift-argument-parser", from: "1.2.0"),
+    .package(url: "https://github.com/apple/swift-algorithms", from: "1.0.0"),
   ],
   targets: [
     .executableTarget(
       name: "Puzzle",
       dependencies: [
-        .product(name: "ArgumentParser", package: "swift-argument-parser")
+        .product(name: "ArgumentParser", package: "swift-argument-parser"),
+        .product(name: "Algorithms", package: "swift-algorithms"),
       ]),
     .testTarget(name: "PuzzleTests", dependencies: ["Puzzle"]),
   ]
